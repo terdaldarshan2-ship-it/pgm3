bbcdd13f8a5498eee27aa4129ae2896b6ebc8bc1
terdaldarshan2-ship-it <terdaldarshan2@gmail.com>
2026-09-15 21:56:38 +0530

    Update pgm3.py

---
+++ pgm3.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print(f"a before the swap:{a}")
+print(f"b before the swap:{b}")
+c=a
+a=b
+b=c
+print(f"a after the swap:{a}")
+print(f"b after the swap:{b}")
